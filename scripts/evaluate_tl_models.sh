5fa3fdf2b767d4080d7b26b16319032bd8877a61
Mateo Baričević <mateo.baricevic@gmail.com>
2022-11-17 22:58:49 +0100

    Fix evaluate tl models script folder structure

--- scripts/evaluate_tl_models.sh
+++ scripts/evaluate_tl_models.sh
@@ -1,5 +1,5 @@
-python evaluate.py ravdess speech 1 > results/ravdess/speech_1/evaluation.txt
-python evaluate.py ravdess speech 2 > results/ravdess/speech_2/evaluation.txt
-python evaluate.py ravdess speech 3 > results/ravdess/speech_3/evaluation.txt
-python evaluate.py ravdess speech 4 > results/ravdess/speech_4/evaluation.txt
-python evaluate.py ravdess speech 5 > results/ravdess/speech_5/evaluation.txt
+python evaluate.py ravdess speech 1 > results/ravdess/speech/1_layers/evaluation.txt
+python evaluate.py ravdess speech 2 > results/ravdess/speech/2_layers/evaluation.txt
+python evaluate.py ravdess speech 3 > results/ravdess/speech/3_layers/evaluation.txt
+python evaluate.py ravdess speech 4 > results/ravdess/speech/4_layers/evaluation.txt
+python evaluate.py ravdess speech 5 > results/ravdess/speech/5_layers/evaluation.txt
